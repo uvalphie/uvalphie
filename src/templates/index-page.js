617438c251a27f5fd99d9b51c0f9c737a7d19e23
minjea-kim <minjeakim.mjk@gmail.com>
2020-09-03 03:25:07 -0400

    Added landing index

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -1,48 +1,51 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import { Link, graphql } from 'gatsby'
-import Layout from '../components/Layout'
+import React from "react";
+import PropTypes from "prop-types";
+import { Link, graphql } from "gatsby";
+import Layout from "../components/Layout";
 
 // Template variable that helps for organization
-export const IndexPageTemplate = ({
-  image,
-  title,
-  heading,
-  subheading,
-  mainpitch,
-  description,
-  intro,
-}) => (
-  <div>
-    <div
-      className="full-width-image margin-top-0"
-      style={{
-        backgroundImage: `url(${
-          !!image.childImageSharp ? image.childImageSharp.fluid.src : image
-        })`,
-        backgroundPosition: `top left`,
-        backgroundAttachment: `fixed`,
-      }}
-    >
-    </div>
-  </div>
-)
-
+// export const IndexPageTemplate = ({
+//   image,
+//   title,
+//   description,
+// }) => (
+//   <div>
+//     <div
+//       className="full-width-image margin-top-0"
+//       style={{
+//         backgroundImage: `url(${
+//           !!image.childImageSharp ? image.childImageSharp.fluid.src : image
+//         })`,
+//         backgroundPosition: `top left`,
+//         backgroundAttachment: `fixed`,
+//       }}
+//     ></div>
+//   </div>
+// );
 
 // ***WHAT ACTUALLY GETS RENDERED
 const IndexPage = ({ data }) => {
-  const { frontmatter } = data.markdownRemark
-
+  const { landing } = useStaticQuery(
+    graphql`
+      query IndexPageTemplate {
+      allMarkdownRemark(filter: {fileAbsolutePath: {regex: "/landing/.*\\.md$/"}}) {
+        nodes {
+          frontmatter {
+            title
+            description
+            header
+          }
+        }
+      }
+    }
+    `
+  )
   return (
     <Layout>
-      <IndexPageTemplate
-        image={frontmatter.image}
-        title={frontmatter.title}
-        description={frontmatter.description}
-      />
+      <h1>{landing.allMarkdownRemark.nodes}</h1>
     </Layout>
-  )
-}
+  );
+};
 
 // Always good to define your props
 IndexPage.propTypes = {
@@ -51,26 +54,22 @@ IndexPage.propTypes = {
       frontmatter: PropTypes.object,
     }),
   }),
-}
+};
 
-export default IndexPage
-
+export default IndexPage;
 
 // Here's where we fetch the data from our admin page
-export const pageQuery = graphql`
-  query IndexPageTemplate {
-    markdownRemark(frontmatter: { templateKey: { eq: "index-page" } }) {
-      frontmatter {
-        title
-        description
-        image {
-          childImageSharp {
-            fluid(maxWidth: 2048, quality: 100) {
-              ...GatsbyImageSharpFluid
-            }
-          }
-        }
-      }
-    }
-  }
-`
+// export const pageQuery = graphql`
+//   query IndexPageTemplate {
+//   allMarkdownRemark(filter: {fileAbsolutePath: {regex: "/landing/.*\\.md$/"}}) {
+//     nodes {
+//       frontmatter {
+//         title
+//         description
+//         header
+//       }
+//     }
+//   }
+// }
+
+// `;
