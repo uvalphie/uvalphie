fcabe61a04916401993458aca777a58dc85191b2
minjea-kim <minjeakim.mjk@gmail.com>
2020-09-16 13:00:17 -0400

    Edit config file

---
+++ src/components/About/TopSection.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { useStaticQuery, graphql } from "gatsby";
+import "./css/topsection.scss";
+
+const TopSection = () => {
+  // const queryResults = useStaticQuery(
+  //   graphql`
+  //   `
+  // );
+
+  return (
+    <div className="top-section">
+    </div>
+  );
+};
+
+export default TopSection;
