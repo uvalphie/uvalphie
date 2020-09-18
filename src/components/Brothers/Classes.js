82fa58e900cf523e883db3a5931f8f1390a38b16
minjea-kim <minjeakim.mjk@gmail.com>
2020-09-18 02:20:43 -0400

    Add class

--- src/components/Brothers/Classes.js
+++ src/components/Brothers/Classes.js
@@ -1,14 +1,40 @@
 import React from "react";
-import { graphql, useStaticQuery } from 'gatsby'
+import { graphql, useStaticQuery } from "gatsby";
+import "./css/classes.scss";
 
 const Classes = () => {
-  // const { site } = useStaticQuery(
-  //   graphql`
-  //   `
-  // )
-  return (
-    <div>Hello</div>
-  )
-}
+  const queryResults = useStaticQuery(
+    graphql`
+      query {
+        markdownRemark(fileAbsolutePath: { regex: "/brothers/classes.md/" }) {
+          frontmatter {
+            class {
+              class_name
+              member {
+                member
+              }
+            }
+          }
+        }
+      }
+    `
+  );
 
-export default Classes
+  function showClassMembers(members) {
+    console.log("MEMBERS ARE: ", members);
+  };
+
+  const classes = queryResults.markdownRemark.frontmatter.class;
+  console.log(classes);
+  return (
+    <div>
+      <div className="classes">
+        {classes.map((classObject, index) => (
+          <button className="class" onClick={() => showClassMembers(classObject.member)} key={index}>{classObject.class_name}</button>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Classes;
--- src/templates/brothers-page.js
+++ src/templates/brothers-page.js
@@ -1,13 +1,14 @@
 import React from "react";
 import Layout from "../components/Layout";
 import TopSection from "../components/Brothers/TopSection";
+import Classes from "../components/Brothers/Classes";
 // import MidSection from "../components/Landing/MidSection";
 
 const BrothersPage = () => {
   return (
     <Layout>
-      Brothers Page
-      {/* <TopSection /> */}
+      <TopSection />
+      <Classes />
     </Layout>
   );
 };
