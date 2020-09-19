9ecadb04d04df8ff6b030372e3e69b17805752eb
minjea-kim <minjeakim.mjk@gmail.com>
2020-09-19 12:21:00 -0400

    Pre body for classes change

--- src/components/Brothers/Classes.js
+++ src/components/Brothers/Classes.js
@@ -1,9 +1,21 @@
 import React from "react";
 import { graphql, useStaticQuery } from "gatsby";
+import SlideUp from "../SlideUp.js";
 import "./css/classes.scss";
 import "./css/common.scss";
 
 const Classes = () => {
+  const [classInfo, changeInfo] = React.useState(null);
+
+  function showClassMembers(members) {
+    changeInfo("test");
+    console.log("MEMBERS ARE: ", classInfo);
+  }
+
+  function closeSlide() {
+    changeInfo(null);
+  }
+
   const queryResults = useStaticQuery(
     graphql`
       query {
@@ -21,14 +33,15 @@ const Classes = () => {
     `
   );
 
-  function showClassMembers(members) {
-    console.log("MEMBERS ARE: ", members);
-  }
-
   const classes = queryResults.markdownRemark.frontmatter.class;
   console.log(classes);
   return (
     <section class="classes" id="subsection">
+      {classInfo ? (
+        <SlideUp slideDown={closeSlide} information={classInfo}>{classInfo} </SlideUp>
+      ) : (
+        <div></div>
+      )}
       <h2>Classes</h2>
       <div className="classes-list">
         {classes.map((classObject, index) => (
--- src/components/SlideUp.js
+++ src/components/SlideUp.js
@@ -2,12 +2,13 @@ import React from "react";
 import "./css/slideup.scss";
 import { graphql, useStaticQuery } from "gatsby";
 
-const SlideUp = ({slideDown}) => {
+const SlideUp = ({slideDown, information}) => {
   return (
     <div class="slide-up">
-      <div class="close">GODOWN</div>
+      {/* <div class="close">GODOWN</div> */}
       <div class="content">
         <button onClick={slideDown}>Close Menu</button>
+        {information}
       </div>
     </div>
   );
