14f55a57f62746ccf52692620ea8ee9e31cb5a9c
minjea-kim <minjeakim.mjk@gmail.com>
2020-10-28 02:36:07 -0400

    Add chairs

--- src/components/Brothers/Chairs.js
+++ src/components/Brothers/Chairs.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { graphql, useStaticQuery } from "gatsby";
-import "./css/chairs.scss";
+import PositionCarousel from "./PositionCarousel";
 
 const Chairs = () => {
   const queryResults = useStaticQuery(
@@ -16,7 +16,7 @@ const Chairs = () => {
               image {
                 childImageSharp {
                   fluid {
-                    src
+                    ...GatsbyImageSharpFluid_noBase64
                   }
                 }
               }
@@ -26,24 +26,12 @@ const Chairs = () => {
       }
     `
   );
+
+  let positionData = queryResults.markdownRemark.frontmatter.position;
+
   return (
     <section class="chairs" id="subsection">
-      <h2 className="center-align"><span>Chair Positions</span></h2>
-      <div class="image-container">
-        <div class="mini-decorator" id="circle1"></div>
-        <div class="mini-decorator" id="circle2"></div>
-        <div class="mini-decorator" id="circle3"></div>
-        <div class="image">
-          <div class="person">
-            {/* <img src="https://placekitten.com/200/286" /> */}
-          </div>
-          <div class="background">
-            {/* <img src="https://placekitten.com/408/287" /> */}
-          </div>
-        </div>
-      </div>
-      <h3>Position</h3>
-      <p>Position Holder</p>
+      <PositionCarousel data={positionData} />
     </section>
   );
 };
