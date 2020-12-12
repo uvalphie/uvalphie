2ec02628af4f47d0ef33ffa0c2caa5bb7b667ae5
minjea-kim <minjeakim.mjk@gmail.com>
2020-12-12 13:40:32 -0500

    Add scroll btn for carousel

--- src/components/Brothers/PositionCarousel.js
+++ src/components/Brothers/PositionCarousel.js
@@ -6,15 +6,16 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
 const PositionCarousel = (data, carouselAlignment) => {
-  const [currentPositionTitle, updatePositionTitle] = React.useState(data.data[0].position_title);
+  const [currentPositionTitle, updatePositionTitle] = React.useState(
+    data.data[0].position_title
+  );
   const [currentPositionHolder, updatePositionHolder] = React.useState("");
   const [showDropdown, toggleDropdown] = React.useState(false);
+  const [showSlideMenu, toggleSlideMenu] = React.useState(false);
 
   const allPositions = data.data;
-  console.log(data.carouselAlignment);
 
   const [carousel, setCarousel] = React.useState();
-  const slider = React.useRef();
 
   function updatePosition(current) {
     updatePositionTitle(allPositions[current].position_title);
@@ -24,6 +25,7 @@ const PositionCarousel = (data, carouselAlignment) => {
   function slickGoTo(index) {
     carousel.slickGoTo(index);
     toggleDropdown(false);
+    toggleSlideMenu(false);
   }
 
   function nextSlide() {
@@ -38,6 +40,14 @@ const PositionCarousel = (data, carouselAlignment) => {
     toggleDropdown(!showDropdown);
   }
 
+  function slideMenuUp() {
+    toggleSlideMenu(true);
+  }
+
+  function closeSlide() {
+    toggleSlideMenu(false);
+  }
+
   const settings = {
     dots: true,
     infinite: true,
@@ -47,9 +57,65 @@ const PositionCarousel = (data, carouselAlignment) => {
     afterChange: (current) => updatePosition(current),
   };
 
-
   return (
     <div className="positions-container">
+      {showSlideMenu ? (
+        <div className="position-selector" id="mobile">
+          <div className="exit-space" onClick={closeSlide}></div>
+          <div className="wrapper">
+            <button onClick={closeSlide}>
+              <svg
+                width="31"
+                height="11"
+                viewBox="0 0 31 11"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <line
+                  y1="-1"
+                  x2="18.0278"
+                  y2="-1"
+                  transform="matrix(0.83205 0.5547 -0.923455 0.383707 0 1)"
+                  stroke="#FEFEFE"
+                  stroke-width="2"
+                />
+                <line
+                  y1="-1"
+                  x2="18.0278"
+                  y2="-1"
+                  transform="matrix(-0.83205 0.5547 0.923455 0.383707 31 1)"
+                  stroke="#FEFEFE"
+                  stroke-width="2"
+                />
+              </svg>
+            </button>
+            <div className="overflow-scroll">
+              {allPositions.map((position, index) => (
+                <div class="position-wrapper">
+                  {position.position_title == currentPositionTitle ? (
+                    <div>
+                      <button className="position" id="selected">
+                        {position.position_title}
+                      </button>
+                    </div>
+                  ) : (
+                    <div>
+                      <button
+                        className="position"
+                        onClick={() => slickGoTo(index)}
+                      >
+                        {position.position_title}
+                      </button>
+                    </div>
+                  )}
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+      ) : (
+        <div></div>
+      )}
       <div className="align-container" id={data.carouselAlignment}>
         <section className="positions" id="subsection">
           <div class="container">
@@ -120,9 +186,19 @@ const PositionCarousel = (data, carouselAlignment) => {
             <button
               onClick={showDropdownMenu}
               className="position-dropdown-btn"
+              id="desktop"
             >
               {currentPositionTitle}
             </button>
+
+            <button
+              onClick={slideMenuUp}
+              className="position-dropdown-btn"
+              id="mobile"
+            >
+              {currentPositionTitle}
+            </button>
+
             <div className="positions-list-wrapper">
               <div className="positions-list">
                 {showDropdown ? (
