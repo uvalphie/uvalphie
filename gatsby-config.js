1815a0b04edfb40a5fe97dbfd733115858c85808
minjea-kim <minjeakim.mjk@gmail.com>
2021-01-24 18:23:12 -0500

    Fix gatsby-config file

--- gatsby-config.js
+++ gatsby-config.js
@@ -38,7 +38,7 @@ module.exports = {
     {
       resolve: "gatsby-source-filesystem",
       options: {
-        path: `${__dirname}/News`,
+        path: `${__dirname}/src/pages/news`,
         name: "News",
       },
     },
